d700a625315659f0d18725bb1e10b24e4e206337
Bojan Dunaj <bojan.dunaj@oradian.com>
2019-05-06 19:23:30 +0200

    Initial commit

---
+++ src/test/scala/example/HelloSpec.scala
@@ -0,0 +1,10 @@
+package example
+
+import com.solnaranu.app.Hello
+import org.scalatest._
+
+class HelloSpec extends FlatSpec with Matchers {
+  "The Hello object" should "say hello" in {
+    Hello.greeting shouldEqual "hello"
+  }
+}
